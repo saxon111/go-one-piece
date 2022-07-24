7f51dc320fad63360f53d42e64bf1c6e3c0ced78
lucas <zhuce11xsy@163.com>
2022-07-25 01:41:08 +0800

    feat(shop-fe):init project fe

--- apps/shop-fe/src/main.ts
+++ apps/shop-fe/src/main.ts
@@ -1,6 +1,6 @@
-import { createApp } from 'vue';
-import './style.css';
-import App from './App.vue';
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 
 console.log(import.meta.env);
-createApp(App).mount('#app');
+createApp(App).mount("#app");
